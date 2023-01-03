450f5480f235738b4b0cda10e96c7a8a5607d6f8
leonildomello42 <leonildo.mello@gmail.com>
2023-01-03 15:58:38 -0300

    Definindo beans com Component

---
+++ especialista-spring-alga/src/cap02_spring_inj_dependencias/aula11-definindo-beans-component/algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafoodapi.di.notificacao;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.stereotype.Component;
+
+@Component
+public class NotificadorEmail {
+
+    public NotificadorEmail() {
+        System.out.println("Construtor chamado notificador Email!");
+    }
+
+    public void notificar(Cliente cliente, String mensagem) {
+        System.out.printf("Notificando %s através do e-mail %s: %s\n",
+                cliente.getNome(), cliente.getEmail(), mensagem);
+    }
+}
---
+++ especialista-spring-alga/src/cap02_spring_inj_dependencias/aula11-definindo-beans-component/algafood-api/src/main/java/com/algaworks/algafoodapi/di/service/AtivacaoClienteService.java
@@ -0,0 +1,17 @@
+package com.algaworks.algafoodapi.di.service;
+
+import com.algaworks.algafoodapi.di.modelo.Cliente;
+import com.algaworks.algafoodapi.di.notificacao.NotificadorEmail;
+import org.springframework.stereotype.Component;
+
+@Component
+public class AtivacaoClienteService {
+
+    private NotificadorEmail notificador;
+
+    public void ativar(Cliente cliente) {
+
+        cliente.ativar();
+        notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+}
