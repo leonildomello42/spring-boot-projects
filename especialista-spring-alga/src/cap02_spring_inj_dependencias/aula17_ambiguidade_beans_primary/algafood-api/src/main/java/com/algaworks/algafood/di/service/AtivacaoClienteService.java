62bc5303e960c75a1278df809caf7b15f92472e4
leonildomello42 <leonildo.mello@gmail.com>
2023-01-04 17:50:31 -0300

    Ambiguidade de beans Primary

---
+++ especialista-spring-alga/src/cap02_spring_inj_dependencias/aula17_ambiguidade_beans_primary/algafood-api/src/main/java/com/algaworks/algafood/di/service/AtivacaoClienteService.java
@@ -0,0 +1,23 @@
+package com.algaworks.algafood.di.service;
+
+import com.algaworks.algafood.di.modelo.Cliente;
+import com.algaworks.algafood.di.notificacao.Notificador;
+import com.algaworks.algafood.di.notificacao.NotificadorEmail;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Component
+public class AtivacaoClienteService {
+
+    @Autowired(required = false)
+    private Notificador notificador;
+
+    public void ativer(Cliente cliente){
+
+        cliente.ativar();
+        this.notificador.notificar(cliente, "Seu cadastro no sistema está ativo!");
+    }
+}
