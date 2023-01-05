c56773cd5ecda0e82c3eb7a6b3b92aaa472a3b4d
leonildomello42 <leonildo.mello@gmail.com>
2023-01-05 00:04:02 -0300

    Removendo imports não utilizados

--- especialista-spring-alga/src/cap02_spring_inj_dependencias/aula27_spring_profiles/algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
+++ especialista-spring-alga/src/cap02_spring_inj_dependencias/aula27_spring_profiles/algafood-api/src/main/java/com/algaworks/algafoodapi/di/notificacao/NotificadorEmail.java
@@ -1,13 +1,8 @@
 package com.algaworks.algafoodapi.di.notificacao;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.context.annotation.Primary;
-import org.springframework.context.annotation.Profile;
-import org.springframework.stereotype.Component;
-
 import com.algaworks.algafoodapi.di.modelo.Cliente;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
 
 //@Profile("prod")
