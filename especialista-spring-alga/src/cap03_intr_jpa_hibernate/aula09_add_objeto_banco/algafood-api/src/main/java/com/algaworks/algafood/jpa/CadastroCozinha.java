6724eaf76d6c96bce5efc2a35404d4ec68615f7d
leonildomello42 <leonildo.mello@gmail.com>
2023-01-06 00:33:36 -0300

    Inserindo objetos no banco de dados

---
+++ especialista-spring-alga/src/cap03_intr_jpa_hibernate/aula09_add_objeto_banco/algafood-api/src/main/java/com/algaworks/algafood/jpa/CadastroCozinha.java
@@ -0,0 +1,13 @@
+package com.algaworks.algafood.jpa;
+
+
+import com.algaworks.algafood.domain.model.Cozinha;
+
+import java.util.List;
+
+public interface CadastroCozinha {
+
+    List<Cozinha> listar();
+
+    Cozinha adicionar(Cozinha cozinha);
+}
