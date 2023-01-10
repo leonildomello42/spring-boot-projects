c2d618113dfa53de577a2f01c78efab75d62570a
leonildomello42 <leonildo.mello@gmail.com>
2023-01-10 17:39:18 -0300

    Lombok

---
+++ especialista-spring-alga/src/cap03_intr_jpa_hibernate/aula15_lombok/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,25 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Entity
+@Table(schema = "algafood", name = "cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Cozinha implements Serializable {
+
+    private static final long serialVersionUID = -6920639314179796384L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nome", length = 255)
+    private String name;
+
+}
