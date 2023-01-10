a02ee6edecf748f4675c9569ecf126195bc53063
leonildomello42 <leonildo.mello@gmail.com>
2023-01-10 17:44:59 -0300

    Lombok Restaurante

---
+++ especialista-spring-alga/src/cap03_intr_jpa_hibernate/aula16_lombok_restaurante/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,31 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.util.Objects;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Table(schema = "algafood", name = "restaurante")
+public class Restaurante implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String nome;
+
+    @Column(name = "taxa_frete")
+    private BigDecimal taxaFrete;
+
+}
