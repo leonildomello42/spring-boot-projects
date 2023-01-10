498bf218ac2f261510922e68e91d84d783d197ec
leonildomello42 <leonildo.mello@gmail.com>
2023-01-10 20:17:29 -0300

    Modelando GET Cozinhas

---
+++ especialista-spring-alga/src/cap4_rest_spring/aula10_resource_get_cozinhas/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Estado.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Estado implements Serializable {
+
+    @Serial
+    private static final long serialVersionUID = 334343008529406395L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+}
+
