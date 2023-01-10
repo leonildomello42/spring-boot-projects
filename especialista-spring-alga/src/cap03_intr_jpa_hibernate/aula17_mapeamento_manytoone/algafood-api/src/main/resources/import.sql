00c36f11123678f61ca59f455ba81f7723d908f3
leonildomello42 <leonildo.mello@gmail.com>
2023-01-10 18:12:04 -0300

    Mapeamento ManyToOne

---
+++ especialista-spring-alga/src/cap03_intr_jpa_hibernate/aula17_mapeamento_manytoone/algafood-api/src/main/resources/import.sql
@@ -0,0 +1,6 @@
+INSERT INTO cozinha (nome, id) VALUES ('Tailandesa', 1);
+INSERT INTO cozinha (nome, id) VALUES ('Indiana', 2);
+
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Gourmet', 10, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Thai Delivery', 9.50, 1);
+INSERT INTO restaurante (nome, taxa_frete, cozinha_id) VALUES ('Tuk Tuk Comida Indiana', 15, 2);
