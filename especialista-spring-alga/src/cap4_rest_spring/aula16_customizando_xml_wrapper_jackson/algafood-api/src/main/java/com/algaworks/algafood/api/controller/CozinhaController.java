99a649da274a52b03b4c6183f1680270bb7a75f0
leonildomello42 <leonildo.mello@gmail.com>
2023-01-11 19:08:22 -0300

    Customizando a representação em XML com Wrapper e anotações do Jackson

---
+++ especialista-spring-alga/src/cap4_rest_spring/aula16_customizando_xml_wrapper_jackson/algafood-api/src/main/java/com/algaworks/algafood/api/controller/CozinhaController.java
@@ -0,0 +1,53 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.api.model.CozinhaXmlWrapper;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+//@Controller
+//@ResponseBody
+
+@RestController
+//@RequestMapping(value = "/cozinhas", produces = MediaType.APPLICATION_JSON_VALUE)
+@RequestMapping("/cozinhas")
+public class CozinhaController {
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+//    @GetMapping(produces = "application/json")
+//    @GetMapping(produces = {MediaType.APPLICATION_JSON_VALUE, MediaType.APPLICATION_XML_VALUE})
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public List<Cozinha> listar(){
+        return cozinhaRepository.listar();
+    }
+
+    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+    public CozinhaXmlWrapper listarXml(){
+        return new CozinhaXmlWrapper(cozinhaRepository.listar());
+    }
+
+//    @GetMapping(produces = MediaType.APPLICATION_XML_VALUE)
+//    public List<Cozinha> listar2(){
+//        System.out.println("Listar 2");
+//        return cozinhaRepository.listar();
+//    }
+
+/*
+    @GetMapping(value = "/{cozinhaId}")
+    public Cozinha buscar(@PathVariable("cozinhaId") Long id) {
+        return cozinhaRepository.buscar(id);
+    }
+*/
+
+    @GetMapping(value = "/{cozinhaId}")
+    public Cozinha buscar(@PathVariable Long cozinhaId) {
+        return cozinhaRepository.buscar(cozinhaId);
+    }
+}
