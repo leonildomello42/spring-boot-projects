fce86702d5ad39112440c83c8e19664e2ee3c48c
leonildomello42 <leonildo.mello@gmail.com>
2023-01-10 21:59:14 -0300

    Consultando recurso unico PathVariable

---
+++ especialista-spring-alga/src/cap4_rest_spring/aula14_get_singleton_resource_pathvariable/algafood-api/src/main/java/com/algaworks/algafood/infrastructure/repository/CozinhaRepositoryImpl.java
@@ -0,0 +1,44 @@
+package com.algaworks.algafood.infrastructure.repository;
+
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import java.util.List;
+
+@Component
+public class CozinhaRepositoryImpl implements CozinhaRepository {
+
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Override
+    public List<Cozinha> listar() {
+
+        TypedQuery<Cozinha> query = entityManager.createQuery("FROM Cozinha", Cozinha.class);
+        return query.getResultList();
+    }
+
+    @Override
+    @Transactional
+    public Cozinha salvar(Cozinha cozinha) {
+        return  entityManager.merge(cozinha);
+    }
+
+    @Override
+    public Cozinha buscar(Long id) {
+        return entityManager.find(Cozinha.class, id);
+    }
+
+    @Override
+    @Transactional
+    public void remover(Cozinha cozinha) {
+        cozinha = buscar(cozinha.getId());
+        entityManager.remove(cozinha);
+    }
+}
