32def1414746887b14283b15fd302a29367258c3
leonildomello42 <leonildo.mello@gmail.com>
2023-01-13 20:51:03 -0300

    Modelando e implementando a atualização de recursos de restaurantes

---
+++ especialista-spring-alga/src/cap4_rest_spring/aula30_31_put_restaurante/algafood-api/src/main/java/com/algaworks/algafood/jpa/controller/AlteracaoCozinhaMain.java
@@ -0,0 +1,37 @@
+package com.algaworks.algafood.jpa.controller;
+
+
+
+import com.algaworks.algafood.AlgafoodApiApplication;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.boot.WebApplicationType;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.context.ApplicationContext;
+
+import java.util.List;
+
+public class AlteracaoCozinhaMain {
+
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = new SpringApplicationBuilder(AlgafoodApiApplication.class)
+                .web(WebApplicationType.NONE)
+                .run(args);
+
+        CozinhaRepository cadastroCozinha = applicationContext.getBean(CozinhaRepository.class);
+
+        Cozinha cozinha = new Cozinha();
+        cozinha.setId(1L);
+        cozinha.setNome("Brasileira");
+
+
+        cadastroCozinha.salvar(cozinha);
+
+        List<Cozinha> cozinhas = cadastroCozinha.listar();
+
+        for (Cozinha coz : cozinhas) {
+            System.out.printf("%d - %s\n", coz.getId(), coz.getNome());
+        }
+    }
+}
