18ee7ea6e8b94132d3b7e1c07ec3f6e5f6a96a88
leonildomello42 <leonildo.mello@gmail.com>
2023-01-21 15:38:56 -0300

    Refatorando repositorios

---
+++ especialista-spring-alga/src/cap05_spring_data_jpa/aula05_refatorando_repositorios_jpa/algafood-api/src/main/java/com/algaworks/algafood/api/controller/TesteController.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.api.controller;
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/teste")
+public class TesteController {
+
+    @Autowired
+    CozinhaRepository cozinhaRepository;
+
+//    @GetMapping("/cozinhas/por-nome/{nome}")
+//    public List<Cozinha> cozinhaPorNome(@PathVariable  String nome){
+//        return cozinhaRepository.consultarPorNome(nome);
+//    }
+
+//    @GetMapping("/cozinhas/por-nome")
+//    public List<Cozinha> cozinhaPorNome(@RequestParam("nome") String nome){
+//        return cozinhaRepository.consultarPorNome(nome);
+//    }
+
+}
--- especialista-spring-alga/src/cap05_spring_data_jpa/aula05_refatorando_repositorios_jpa/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
+++ especialista-spring-alga/src/cap05_spring_data_jpa/aula05_refatorando_repositorios_jpa/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -1,12 +1,14 @@
 package com.algaworks.algafood.domain.repository;
 
-import com.algaworks.algafood.domain.model.Permissao;
+
+import com.algaworks.algafood.domain.model.Cozinha;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface PermissaoRepository extends JpaRepository<Permissao, Long> {
+public interface CozinhaRepository extends JpaRepository<Cozinha, Long> {
 
+//    List<Cozinha> consultarPorNome(String nome);
 }
