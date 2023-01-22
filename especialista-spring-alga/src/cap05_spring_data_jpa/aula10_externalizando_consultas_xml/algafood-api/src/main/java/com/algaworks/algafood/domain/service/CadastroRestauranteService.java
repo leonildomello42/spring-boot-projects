ee0f60bd8189252aa05da28f7887beefef32dcc9
leonildomello42 <leonildo.mello@gmail.com>
2023-01-22 13:45:41 -0300

    Externalizando consultas JPQL para um arquivo XML

---
+++ especialista-spring-alga/src/cap05_spring_data_jpa/aula10_externalizando_consultas_xml/algafood-api/src/main/java/com/algaworks/algafood/domain/service/CadastroRestauranteService.java
@@ -0,0 +1,46 @@
+package com.algaworks.algafood.domain.service;
+
+import com.algaworks.algafood.domain.exception.EntidadeNaoEncontradaException;
+import com.algaworks.algafood.domain.model.Cozinha;
+import com.algaworks.algafood.domain.model.Restaurante;
+import com.algaworks.algafood.domain.repository.CozinhaRepository;
+import com.algaworks.algafood.domain.repository.RestauranteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class CadastroRestauranteService {
+
+    @Autowired
+    private RestauranteRepository restauranteRepository;
+
+    @Autowired
+    private CozinhaRepository cozinhaRepository;
+
+
+    public Restaurante salvar(Restaurante restaurante) {
+
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Cozinha cozinha = cozinhaRepository.findById(cozinhaId)
+                .orElseThrow(() -> new EntidadeNaoEncontradaException(
+                        String.format("Não existe cadastro de cozinha com código %d", cozinhaId)));
+
+        restaurante.setCozinha(cozinha);
+        return restauranteRepository.save(restaurante);
+    }
+
+    public Restaurante salvarDois(Restaurante restaurante) {
+
+        Long cozinhaId = restaurante.getCozinha().getId();
+        Optional<Cozinha> cozinha = cozinhaRepository.findById(cozinhaId);
+
+        if (cozinha.isEmpty()) {
+            throw new EntidadeNaoEncontradaException(
+                    String.format("Não existe cadastro de cozinha com código %d", cozinhaId));
+        }
+        restaurante.setCozinha(cozinha.get());
+        return restauranteRepository.save(restaurante);
+    }
+}
