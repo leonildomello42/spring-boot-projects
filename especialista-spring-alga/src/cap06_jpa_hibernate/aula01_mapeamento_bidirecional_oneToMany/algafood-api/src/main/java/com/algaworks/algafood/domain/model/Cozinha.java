a22f1b98aea7a42d6ce579deec24de2dec8b12bc
leonildomello42 <leonildo.mello@gmail.com>
2023-01-27 03:01:51 -0300

    Mapeando relacionamento bidirecional com OneToMany

--- especialista-spring-alga/src/cap06_jpa_hibernate/aula01_mapeamento_bidirecional_oneToMany/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
+++ especialista-spring-alga/src/cap06_jpa_hibernate/aula01_mapeamento_bidirecional_oneToMany/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -1,11 +1,14 @@
 package com.algaworks.algafood.domain.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import com.fasterxml.jackson.annotation.JsonRootName;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import javax.persistence.*;
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "cozinha")
@@ -25,4 +28,8 @@ public class Cozinha implements Serializable {
 //    @JsonIgnore
     @Column(name = "nome", length = 255, nullable = false)
     private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    List<Restaurante> restaurantes = new ArrayList<>();
 }
