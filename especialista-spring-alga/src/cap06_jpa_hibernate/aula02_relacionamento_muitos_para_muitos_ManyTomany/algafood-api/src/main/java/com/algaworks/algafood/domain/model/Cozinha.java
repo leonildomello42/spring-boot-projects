55a4176ea14561537d300d8ead1f3b3014cdfe55
leonildomello42 <leonildo.mello@gmail.com>
2023-01-27 03:19:56 -0300

    Mapeando relacionamento muitos-para-muitos com ManyToMany

---
+++ especialista-spring-alga/src/cap06_jpa_hibernate/aula02_relacionamento_muitos_para_muitos_ManyTomany/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cozinha.java
@@ -0,0 +1,35 @@
+package com.algaworks.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonRootName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "cozinha")
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@JsonRootName("Cozinha") //fica gastronomia no xml
+public class Cozinha implements Serializable {
+
+    private static final long serialVersionUID = -6920639314179796384L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+//    @JsonProperty("titulo")
+//    @JsonIgnore
+    @Column(name = "nome", length = 255, nullable = false)
+    private String nome;
+
+    @JsonIgnore
+    @OneToMany(mappedBy = "cozinha")
+    List<Restaurante> restaurantes = new ArrayList<>();
+}
