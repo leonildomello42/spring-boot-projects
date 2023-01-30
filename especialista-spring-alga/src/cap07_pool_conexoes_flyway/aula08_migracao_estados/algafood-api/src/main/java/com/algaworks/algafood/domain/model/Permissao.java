a8665896fab34e66f075554fa6ec4aae4b7e58ff
leonildomello42 <leonildo.mello@gmail.com>
2023-01-30 19:49:02 -0300

    Criando migrações complexas com remanejamento de dados tabela estados

---
+++ especialista-spring-alga/src/cap07_pool_conexoes_flyway/aula08_migracao_estados/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Permissao.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Permissao implements Serializable{
+
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(nullable = false)
+    private String descricao;
+}
+
