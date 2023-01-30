fab25b21812593856b6d7fc35b443b522db7be2b
leonildomello42 <leonildo.mello@gmail.com>
2023-01-30 20:14:11 -0300

    Criando migração a partir de DDL gerado por schema generation

---
+++ especialista-spring-alga/src/cap07_pool_conexoes_flyway/aula09_ddl_schema_generation/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Restaurante.java
@@ -0,0 +1,75 @@
+package com.algaworks.algafood.domain.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import javax.persistence.*;
+import java.io.Serial;
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Restaurante implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @Column(name = "taxa_frete", nullable = false)
+    private BigDecimal taxaFrete;
+
+    //Muitos restaurantes tem uma cozinha
+    //tudo que termina com ToOne - usa estrategia Eager Loading
+
+//    @JsonIgnore
+//    @JsonIgnoreProperties("hibernateLazyInitializer")
+    @ManyToOne //(fetch = FetchType.LAZY)
+    @JoinColumn(name = "cozinha_id", nullable = false)
+    private Cozinha cozinha;
+
+    @JsonIgnore
+    @Embedded
+    private Endereco endereco;
+
+
+    @JsonIgnore
+    @CreationTimestamp
+    @Column(nullable = false, columnDefinition = "datetime")
+    private LocalDateTime dataCadastro;
+
+    @JsonIgnore
+    @UpdateTimestamp
+    @Column(nullable = false, columnDefinition = "datetime(0)")
+    private LocalDateTime dataAtualizacao;
+
+    @JsonIgnore
+    //mappedBy = "restaurante": qual o nome da propriedade inversa, no caso em produto, temos restaurante
+    @OneToMany(mappedBy = "restaurante")
+    List<Produto> produtos = new ArrayList<>();
+
+
+//    @ManyToMany(fetch = FetchType.EAGER)
+    //tudo que termina com ToMany - usa estrategia Lazy Loading
+//    @JsonIgnore
+    @ManyToMany//(fetch = FetchType.EAGER)
+    @JoinTable(name = "restaurante_forma_pagamento",
+    joinColumns = @JoinColumn(name = "restaurante_id"),
+    inverseJoinColumns = @JoinColumn(name = "forma_pagamento_id"))
+    private List<FormaPagamento> formasPagamentos = new ArrayList<>();
+
+}
