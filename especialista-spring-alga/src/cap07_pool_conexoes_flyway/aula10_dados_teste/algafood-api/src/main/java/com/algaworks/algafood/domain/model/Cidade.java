c5e1d0e50320fecfc54a90b3c6e391bbec8e5422
leonildomello42 <leonildo.mello@gmail.com>
2023-01-30 22:32:19 -0300

    Adicionando dados de testes com callback do Flyway

---
+++ especialista-spring-alga/src/cap07_pool_conexoes_flyway/aula10_dados_teste/algafood-api/src/main/java/com/algaworks/algafood/domain/model/Cidade.java
@@ -0,0 +1,27 @@
+package com.algaworks.algafood.domain.model;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import javax.persistence.*;
+import java.io.Serializable;
+
+@Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
+public class Cidade implements Serializable {
+
+    private static final long serialVersionUID = 1L;
+
+    @EqualsAndHashCode.Include
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String nome;
+
+    @ManyToOne
+    @JoinColumn(nullable = false)
+    private Estado estado;
+}
---
+++ especialista-spring-alga/src/cap07_pool_conexoes_flyway/aula10_dados_teste/algafood-api/src/main/java/com/algaworks/algafood/domain/repository/CozinhaRepository.java
@@ -0,0 +1,29 @@
+package com.algaworks.algafood.domain.repository;
+
+
+import com.algaworks.algafood.domain.model.Cozinha;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface CozinhaRepository extends CustomJpaRepository<Cozinha, Long> {
+
+
+    List<Cozinha> nome(String nome);
+
+    List<Cozinha> findByNome(String nome);
+
+    List<Cozinha> findQualquerCoisaByNome(String nome);
+
+    Optional<Cozinha> findUnicaByNome(String nome);
+
+    //Todas as cozinhas que contain o nome
+    List<Cozinha> findTodasByNomeContaining(String nome);
+
+    boolean existsByNome(String nome);
+}
